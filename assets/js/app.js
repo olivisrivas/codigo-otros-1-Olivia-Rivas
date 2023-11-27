4a649c6fc7a6aef9a5d77de76857cce95e1ba886
Olivia Rivas~ <olivia.rivas.prf@gmail.com>
2023-11-27 17:32:05 -0600

    Revisión

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,27 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $n = document.querySelector('.name'); // Correción de: Se corrigió ('name') -> ('.name') porque está definido por class
+const $b = document.querySelector('.blog'); 
+const $l = document.querySelector('.location'); // Correción de: Se agregó en el archivo HTML el comando " <p class="location"></p> " para que se inter conecten con las instrucciones
 
-function displayUser(username) {
+async function displayUser(username) {
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  try {
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    const data = await response.json();
+    console.log(data);
+    $n.textContent = `${data.name}`;
+    $b.textContent = `${data.blog}`;
+    $l.textContent = `${data.location}`;
+  } catch (err) {
+    handleError(err);
+  }
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`;
 }
 
 displayUser('stolinski').catch(handleError);
